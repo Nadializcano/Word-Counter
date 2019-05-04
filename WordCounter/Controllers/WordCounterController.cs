89c3907ea4e2c88fa11f8f9c83a2535c00924182
Nadia Lizcano <priscila.lizcano@gmail.com>
2019-05-03 17:23:07 -0700

    1st test WordCounterControllerTest pass succesful

--- WordCounter/Controllers/WordCounterController.cs
+++ WordCounter/Controllers/WordCounterController.cs
@@ -1,23 +1,23 @@
-// using Microsoft.AspNetCore.Mvc;
-// using System.Collections.Generic;
-// using WordCounter.Models;
-//
-//
-// namespace WordCounter.Controllers
-// {
-//   public class WordCounterController : Controller
-//   {
-//     [HttpGet("/wordcounter")]
-//    public ActionResult Index()
-//    {
-//      return View();
-//    }
-//
-//     [HttpGet("/wordcounter/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
-//
-//   }
-// }
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Models;
+
+
+namespace WordCounter.Controllers
+{
+  public class WordCounterController : Controller
+  {
+    [HttpGet("/wordcounter")]
+   public ActionResult Index()
+   {
+     return View();
+   }
+
+    [HttpGet("/wordcounter/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+  }
+}
